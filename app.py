3c1c265141b571b99550f95f3d80263c41144857
Shrey-Modi <modi7@wisc.edu>
2023-11-13 19:32:59 -0600

    added some relevant data

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import squarify
+import secrets
+
 
 def main():
     st.title('LSC Infographic: Global Chip Shortage')
 
-    st.text('test treemap')
+    st.write('Chip Demand by Revenue (in billion $)\n\nsource : https://spectrum.ieee.org/chip-shortage')
     
     st.set_option('deprecation.showPyplotGlobalUse', False)
-    frozen = st.slider('Change Frozen Volume', 20, 200, 150)
-    volume = [350, 220, 170, frozen, 50]
-    labels = ['Liquid\n volume: 350k', 'Savoury\n volume: 220k',
-            'Sugar\n volume: 170k', f'Frozen\n volume: {frozen}k',
-            'Non-food\n volume: 50k']
-    color_list = ['#0f7216', '#b2790c', '#ffe9a3',
-                '#f9d4d4', '#d35158', '#ea3033']
+    # frozen = st.slider('Change Frozen Volume', 20, 200, 150)
+    # values = [350, 220, 170, frozen, 50]
+    # labels = ['Liquid\n volume: 350k', 'Savoury\n volume: 220k',
+    #         'Sugar\n volume: 170k', f'Frozen\n volume: {frozen}k',
+    #         'Non-food\n volume: 50k']
 
-    plt.rc('font', size=14)
-    squarify.plot(sizes=volume, label=labels,
-                color=color_list, alpha=0.7)
+    treemap_data = {
+        'Automotive': 39.5,
+        'Consumer': 60.1,
+        'Wireless': 126.7,
+        'Computing': 160.2,
+        'Communication': 36.3,
+        'Industrial': 41.6
+    }
+    color_list = ['#3F1973', '#3407A0', '#9C9FD9',
+                '#402A01', '#BBA26A', '#D6B798']
+
+    plt.rc('font', size=8)
+    squarify.plot(sizes=treemap_data.values() , label=[f"{k}\n{v}" for k, v in treemap_data.items()],
+                 color=[f"#{secrets.token_hex(3)}" for i in treemap_data], alpha=0.7)
     plt.axis('off')
     st.pyplot()
 
